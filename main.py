ba2b3b23e75fb633a95c166bae22c219a91fd41f
dragonloverlord <dragonloverlord@gmail.com>
2015-03-25 05:54:18 -0400

    removed pointless code from main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 __author__ = 'dragonloverlord'
+
+from gi.repository import Gtk
+
+class PadWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="PyPad")
+        self.set_size_request(600, 500)
+
+
+pad = PadWindow()
+pad.connect("delete-event", Gtk.main_quit)
+pad.show_all()
+Gtk.main()
