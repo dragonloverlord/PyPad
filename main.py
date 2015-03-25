a169faed6595375643f5d807b8c99bb2b6d7ae45
dragonloverlord <dragonloverlord@gmail.com>
2015-03-25 08:12:19 -0400

    added code for the menubar and file menuitem.

--- main.py
+++ main.py
@@ -7,6 +7,17 @@ class PadWindow(Gtk.Window):
         Gtk.Window.__init__(self, title="PyPad")
         self.set_size_request(600, 500)
 
+        self.main_grid = Gtk.Grid()
+        self.main_grid.set_hexpand = True
+        self.add(self.main_grid)
+
+        self.main_grid.menubar = Gtk.MenuBar()
+        self.main_grid.menubar.set_hexpand = True
+        self.main_grid.attach(self.main_grid.menubar, 0, 0, 1, 1)
+
+        self.main_grid.menubar.file_menu = Gtk.MenuItem(label="File")
+        self.main_grid.menubar.append(self.main_grid.menubar.file_menu)
+
 
 pad = PadWindow()
 pad.connect("delete-event", Gtk.main_quit)
