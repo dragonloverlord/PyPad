256ab360c10c23fe0ae3ea36482fd93aada931f4
dragonloverlord <dragonloverlord@gmail.com>
2015-03-25 08:17:32 -0400

    fixed a few errors.

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ class PadWindow(Gtk.Window):
         self.set_size_request(600, 500)
 
         self.main_grid = Gtk.Grid()
-        self.main_grid.set_hexpand = True
+        self.main_grid.set_hexpand(True)
         self.add(self.main_grid)
 
         self.main_grid.menubar = Gtk.MenuBar()
-        self.main_grid.menubar.set_hexpand = True
+        self.main_grid.menubar.set_hexpand(True)
         self.main_grid.attach(self.main_grid.menubar, 0, 0, 1, 1)
 
         self.main_grid.menubar.file_menu = Gtk.MenuItem(label="File")
