0731e50a3f0393d14b79d2d460b113f250f867ec
dragonloverlord <dragonloverlord@gmail.com>
2015-03-25 08:55:56 -0400

    built onto the menubar.

--- main.py
+++ main.py
@@ -16,7 +16,20 @@ class PadWindow(Gtk.Window):
         self.main_grid.attach(self.main_grid.menubar, 0, 0, 1, 1)
 
         self.main_grid.menubar.file_menu = Gtk.MenuItem(label="File")
+        self.main_grid.menubar.file_menu.submenu = Gtk.Menu()
+        self.main_grid.menubar.file_menu.submenu.new_file = Gtk.MenuItem(label="New File")
+        self.main_grid.menubar.file_menu.submenu.attach(self.main_grid.menubar.file_menu.submenu.new_file, 0, 1, 0, 1)
+        self.main_grid.menubar.file_menu.set_submenu(self.main_grid.menubar.file_menu.submenu)
         self.main_grid.menubar.append(self.main_grid.menubar.file_menu)
+
+        self.main_grid.menubar.edit_menu = Gtk.MenuItem(label="Edit")
+        self.main_grid.menubar.append(self.main_grid.menubar.edit_menu)
+
+        self.main_grid.menubar.options_menu = Gtk.MenuItem(label="Options")
+        self.main_grid.menubar.append(self.main_grid.menubar.options_menu)
+
+        self.main_grid.menubar.help_menu = Gtk.MenuItem(label="Help")
+        self.main_grid.menubar.append(self.main_grid.menubar.help_menu)
 
 
 pad = PadWindow()
